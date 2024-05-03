b8707a753e2a666a0932ed150aae78c2d9abc993
Mariam Maria <mariammaria38355@gmail.com>
2024-05-03 21:54:44 +0600

    Started developing backend part

---
+++ admin/partials/footer.php
@@ -0,0 +1,10 @@
+        <!--Footer section starts -->
+        <div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2024 All rights reserved,NIGHTQUEEN. Developed by - <a href="https://www.linkedin.com/in/findmariammariaa/">Mariam Maria</p></a>
+            </div>
+
+        </div>
+        <!--Footer section ends -->
+    </body>
+</html>
